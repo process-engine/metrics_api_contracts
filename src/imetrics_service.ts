1ebef5bf4baedb7a9970298754b4434e5bec4908
ElRaptorus <christian.werner@5minds.de>
2018-08-15 15:15:28 +0200

    Add Review notes

--- src/imetrics_service.ts
+++ src/imetrics_service.ts
@@ -2,6 +2,7 @@
  * Contains functions for recording metrics relating to process instances and flow node instances.
  */
 export interface IMetricsService {
+
   /**
    * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
    * The metric contains performance related data, gathered at the beginning of the process models/flow node instances execution.
@@ -11,6 +12,7 @@ export interface IMetricsService {
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
   writeOnEnter(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+
   /**
    * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
    * The metric contains performance related data, gathered after the process models/flow node instances finished execution.
@@ -20,6 +22,7 @@ export interface IMetricsService {
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
   writeOnExit(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+
   /**
    * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
    * The metric contains performance related data, gathered when the process models/flow node instance encountered an error.
@@ -29,6 +32,7 @@ export interface IMetricsService {
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
   writeOnError(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+
   /**
    * For FlowNodeInstances only.
    * Writes a metric entry for a flow node instance of a process instance.
@@ -39,6 +43,7 @@ export interface IMetricsService {
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
   writeOnSuspend(processModeldId: string, correlationId: string, flowNodeInstanceId: string): void;
+
   /**
    * For FlowNodeInstances only.
    * Writes a metric entry for a flow node instance of a process instance.
