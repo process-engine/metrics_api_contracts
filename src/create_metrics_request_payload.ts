50f6799a51411c8810a3f84ad29425dcfe13ef31
ElRaptorus <christian.werner@5minds.de>
2018-08-16 15:10:20 +0200

    Add timestamp to metric creation functions

---
+++ src/create_metrics_request_payload.ts
@@ -0,0 +1,3 @@
+export class CreateMetricsRequestPayload {
+  public timestamp: Date;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './imetrics_service';
+export * from './create_metrics_request_payload';
 export * from './rest_settings';
