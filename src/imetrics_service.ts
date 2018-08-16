f9d0946ae172a4ab1eb34e2901417dfc26e8afe2
ElRaptorus <christian.werner@5minds.de>
2018-08-16 12:04:34 +0200

    Add additional methods for gathering process model metrics

--- src/imetrics_service.ts
+++ src/imetrics_service.ts
@@ -4,54 +4,79 @@
 export interface IMetricsService {
 
   /**
-   * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
-   * The metric contains performance related data, gathered at the beginning of the process models/flow node instances execution.
-   *
+   * Writes a metric entry for a process model within a correlation.
+   * The metric contains performance related data, gathered at the beginning of the process models execution.
+   * @async
+   * @param processModeldId     The process model id for which to write an onEnter-type metric.
+   * @param correlationId       The correlation id for which to create an onEnter-type metric.
+   */
+  writeOnProcessStarted(processModeldId: string, correlationId: string): Promise<void>;
+
+  /**
+   * Writes a metric entry for a process model within a correlation.
+   * The metric contains performance related data, gathered after the process model finished execution.
+   * @async
+   * @param processModeldId     The process model id for which to write an onEnter-type metric.
+   * @param correlationId       The correlation id for which to create an onEnter-type metric.
+   */
+  writeOnProcessFinished(processModeldId: string, correlationId: string): Promise<void>;
+
+  /**
+   * Writes a metric entry for a process model within a correlation.
+   * The metric contains performance related data, gathered when the process model encountered an error.
+   * @async
+   * @param processModeldId     The process model id for which to write an onEnter-type metric.
+   * @param correlationId       The correlation id for which to create an onEnter-type metric.
+   */
+  writeOnProcessError(processModeldId: string, correlationId: string): Promise<void>;
+
+  /**
+   * Writes a metric entry for a flow node instance of a specific process model within a correlation.
+   * The metric contains performance related data, gathered at the beginning of the flow node instances execution.
+   * @async
    * @param processModeldId     The process model id for which to write an onEnter-type metric.
    * @param correlationId       The correlation id for which to create an onEnter-type metric.
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
-  writeOnEnter(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+  writeOnEnter(processModeldId: string, correlationId: string, flowNodeInstanceId: string): Promise<void>;
 
   /**
-   * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
-   * The metric contains performance related data, gathered after the process models/flow node instances finished execution.
-   *
+   * Writes a metric entry for a flow node instance of a specific process model within a correlation.
+   * The metric contains performance related data, gathered after the flow node instances finished execution.
+   * @async
    * @param processModeldId     The process model id for which to write an onEnter-type metric.
    * @param correlationId       The correlation id for which to create an onEnter-type metric.
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
-  writeOnExit(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+  writeOnExit(processModeldId: string, correlationId: string, flowNodeInstanceId: string): Promise<void>;
 
   /**
-   * Writes a metric entry for a process instance, or a specific flow node instance of that process instance.
-   * The metric contains performance related data, gathered when the process models/flow node instance encountered an error.
-   *
+   * Writes a metric entry for a flow node instance of a specific process model within a correlation.
+   * The metric contains performance related data, gathered when the flow node instance encountered an error.
+   * @async
    * @param processModeldId     The process model id for which to write an onEnter-type metric.
    * @param correlationId       The correlation id for which to create an onEnter-type metric.
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
-  writeOnError(processModeldId: string, correlationId: string, flowNodeInstanceId?: string): void;
+  writeOnError(processModeldId: string, correlationId: string, flowNodeInstanceId: string): Promise<void>;
 
   /**
-   * For FlowNodeInstances only.
    * Writes a metric entry for a flow node instance of a process instance.
    * The metric contains performance related data, gathered when the flow node instance was suspended.
-   *
+   * @async
    * @param processModeldId     The process model id for which to write an onEnter-type metric.
    * @param correlationId       The correlation id for which to create an onEnter-type metric.
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
-  writeOnSuspend(processModeldId: string, correlationId: string, flowNodeInstanceId: string): void;
+  writeOnSuspend(processModeldId: string, correlationId: string, flowNodeInstanceId: string): Promise<void>;
 
   /**
-   * For FlowNodeInstances only.
    * Writes a metric entry for a flow node instance of a process instance.
    * The metric contains performance related data, gathered when the flow node instance was resumed.
-   *
+   * @async
    * @param processModeldId     The process model id for which to write an onEnter-type metric.
    * @param correlationId       The correlation id for which to create an onEnter-type metric.
    * @param flowNodeInstanceId  The flow node instance for which to create an onEnter-type metric.
    */
-  writeOnResume(processModeldId: string, correlationId: string, flowNodeInstanceId: string): void;
+  writeOnResume(processModeldId: string, correlationId: string, flowNodeInstanceId: string): Promise<void>;
 }
