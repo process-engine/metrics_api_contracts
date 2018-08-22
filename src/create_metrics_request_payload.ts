45196d60ffbf5be1e77a9d53a581055affd39174
ElRaptorus <christian.werner@5minds.de>
2018-08-22 15:24:51 +0200

    Add flowNodeInstanceId to request payload

--- src/create_metrics_request_payload.ts
+++ src/create_metrics_request_payload.ts
@@ -2,5 +2,6 @@
  * Contains the payload that gets send with a HTTP request to record metrics.
  */
 export class CreateMetricsRequestPayload {
+  public flowNodeInstanceId?: string;
   public timestamp: Date;
 }
